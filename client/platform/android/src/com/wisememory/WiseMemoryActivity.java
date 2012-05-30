f39f31289f2d0de71125c1662c9df1f2e4f6c6e5
Youngbok Yoon <bok@wisememory.com>
2012-05-30 20:40:44 +0900

    Initial

---
+++ client/platform/android/src/com/wisememory/WiseMemoryActivity.java
@@ -0,0 +1,15 @@
+package com.wisememory;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class WiseMemoryActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+        
+    }
+}
